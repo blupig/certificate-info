6f0162287e6ebf058c3fb12717d9ad916af01140
Yunzhu Li <yunzhu.li@nytimes.com>
2016-12-09 14:35:57 -0500

    First working version

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -18,23 +18,33 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
 // Update badge text to validation level
 function badgeValidationLevel(url) {
+  // Check if https
+  if (url.substring(0, 8) !== 'https://') {
+    chrome.browserAction.setBadgeText({text: ''});
+    return;
+  }
+
   // Set badge if already cached
-  if (typeof cached_levels['url'] !== 'undefined') {
-    chrome.browserAction.setBadgeText({text: cached_levels[url][100]});
+  if (typeof cached_levels[url] !== 'undefined') {
+    chrome.browserAction.setBadgeText({text: cached_levels[url]});
     return;
   }
 
   // Make request
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        var cert_info = JSON.parse(this.responseText);
-        var lvl = cert_info['validation_level'];
-        cached_levels['url'] = lvl;
-        chrome.browserAction.setBadgeText({text: lvl[100]});
-      }
+    if (typeof this.responseText === 'undefined' ||
+        this.responseText.length === 0) {
+
+      chrome.browserAction.setBadgeText({text: ''});
+      return;
+    }
+    var cert_info = JSON.parse(this.responseText);
+    var lvl = cert_info['validation_level'];
+    cached_levels[url] = lvl;
+    chrome.browserAction.setBadgeText({text: lvl});
   };
 
-  xhr.open("GET", "https://api.blupig.net/cert-info/?url=" + encodeURIComponent(url), true);
+  xhr.open("GET", "http://192.81.217.28/?url=" + encodeURIComponent(url), true);
   xhr.send();
 }
