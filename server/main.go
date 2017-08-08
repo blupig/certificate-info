02839dc39ae5f6c667a1f7d70d50ea7926f336a9
Yunzhu Li <contact@yunzhu.li>
2017-08-07 22:34:54 -0400

    Server API improvement

--- server/main.go
+++ server/main.go
@@ -23,8 +23,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 // rootHandler handles requests to /
@@ -36,48 +39,85 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "ok")
 }
 
-func certHandler(w http.ResponseWriter, r *http.Request) {
+func validateHandler(w http.ResponseWriter, r *http.Request) {
 	hostname := r.URL.Query().Get("host")
-
-	// Create TLS connection
-	conn, err := tls.Dial("tcp", hostname+":443", nil)
-	if err != nil {
-		fmt.Fprint(w, "{\"message\":\"Failed to establish TLS connection: "+err.Error()+"\"}")
+	if len(hostname) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "{\"message\":\"Invalid parameters\"}")
 		return
 	}
-	defer conn.Close()
 
-	// Get certificate info
-	cert := conn.ConnectionState().PeerCertificates[0]
-	subj := cert.Subject
-	orgs := subj.Organization
+	// Marshal and write response
+	result := validateHost(hostname)
+	m, _ := json.Marshal(result)
+	fmt.Fprint(w, string(m))
+}
 
-	// Default validation level
-	validationLevel := "Domain Control Validated"
-	validationLevelShort := "DV"
+// Validate host and return response
+func validateHost(hostname string) map[string]string {
+	// Result
+	validationResult := ""
+	validationResultShort := ""
+	resultColor := ""
+	message := ""
+
+	// Cert info
 	certOrganization := ""
-	message := "The website operator's control over this domain has been validated."
 
-	// Set to IV if cert has O field
-	if len(orgs) > 0 {
-		validationLevel = "Identity Validated"
-		validationLevelShort = "IV"
-		certOrganization = orgs[0]
-		message = "The website operator's identity (organization or individual) has been validated."
+	// Add port number if not already
+	if !strings.Contains(hostname, ":") {
+		hostname += ":443"
+	}
+
+	// Create TLS connection
+	dialer := new(net.Dialer)
+	dialer.Timeout = 5 * time.Second // Timeout: 5 seconds
+	conn, err := tls.DialWithDialer(dialer, "tcp", hostname, nil)
+	if err != nil {
+		// Connection failed
+		validationResult = "Not Validated"
+		validationResultShort = "!"
+		resultColor = "red"
+		message = err.Error()
+
+	} else {
+		defer conn.Close()
+
+		// Get certificate info
+		cert := conn.ConnectionState().PeerCertificates[0]
+		subj := cert.Subject
+		orgs := subj.Organization
+
+		// Default validation level
+		validationResult = "Domain Control Validated"
+		validationResultShort = "DV"
+		certOrganization = ""
+		resultColor = "yellow"
+		message = "The website operator's control over this domain has been validated."
+
+		// Set to IV if cert has O field
+		if len(orgs) > 0 {
+			validationResult = "Identity Validated"
+			validationResultShort = "IV"
+			certOrganization = orgs[0]
+			resultColor = "blue"
+			message = "The website operator's identity (organization or individual) has been validated."
+		}
 	}
 
 	// Result object
 	result := map[string]string{
-		"validation_level":       validationLevel,
-		"validation_level_short": validationLevelShort,
-		"organization":           certOrganization,
-		"message":                message,
+		"validation_result":       validationResult,
+		"validation_result_short": validationResultShort,
+		"validation_level":        validationResult,
+		"validation_level_short":  validationResultShort,
+		"cert_organization":       certOrganization,
+		"organization":            certOrganization,
+		"result_color":            resultColor,
+		"message":                 message,
 	}
 
-	// Marshal and write response
-	m, _ := json.Marshal(result)
-	fmt.Fprint(w, string(m))
-	return
+	return result
 }
 
 // Application entry point
@@ -91,7 +131,8 @@ func main() {
 	// Routes
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/health", healthHandler)
-	http.HandleFunc("/cert", certHandler)
+	http.HandleFunc("/cert", validateHandler)
+	http.HandleFunc("/validate", validateHandler)
 
 	// Start serving
 	log.Println("Starting service...")
