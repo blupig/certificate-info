dd2bf1c426e5db607e6146b282d2440e7cb32908
Yunzhu Li <contact@yunzhu.li>
2017-08-25 18:51:23 -0400

    Identify EV certs (#12)

--- server/main.go
+++ server/main.go
@@ -19,6 +19,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
@@ -27,9 +28,112 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
+
+var ev_oids = map[string]bool{
+	// Actalis
+	"1.3.159.1.17.1": true,
+
+	// AffirmTrust
+	"1.3.6.1.4.1.34697.2.1": true,
+	"1.3.6.1.4.1.34697.2.2": true,
+	"1.3.6.1.4.1.34697.2.3": true,
+	"1.3.6.1.4.1.34697.2.4": true,
+
+	// A-Trust
+	"1.2.40.0.17.1.22": true,
+
+	// Buypass
+	"2.16.578.1.26.1.3.3": true,
+
+	// Camerfirma
+	"1.3.6.1.4.1.17326.10.14.2.1.2": true,
+	"1.3.6.1.4.1.17326.10.8.12.1.2": true,
+
+	// Comodo Group
+	"1.3.6.1.4.1.6449.1.2.1.5.1": true,
+
+	// DigiCert
+	"2.16.840.1.114412.2.1":     true,
+	"2.16.840.1.114412.1.3.0.2": true,
+
+	// E-Tugra
+	"2.16.792.3.0.4.1.1.4": true,
+
+	// Entrust
+	"2.16.840.1.114028.10.1.2": true,
+
+	// ETSI
+	"0.4.0.2042.1.4": true,
+	"0.4.0.2042.1.5": true,
+
+	// Firmaprofesional
+	"1.3.6.1.4.1.13177.10.1.3.10": true,
+
+	// GeoTrust
+	"1.3.6.1.4.1.14370.1.6": true,
+
+	// GlobalSign
+	"1.3.6.1.4.1.4146.1.1": true,
+
+	// Go Daddy
+	"2.16.840.1.114413.1.7.23.3": true,
+
+	// Izenpe
+	"1.3.6.1.4.1.14777.6.1.1": true,
+
+	// Kamu Sertifikasyon Merkezi
+	"2.16.792.1.2.1.1.5.7.1.9": true,
+
+	// Logius PKIoverheid
+	"2.16.528.1.1003.1.2.7": true,
+
+	// Network Solutions
+	"1.3.6.1.4.1.782.1.2.1.8.1": true,
+
+	// OpenTrust/DocuSign France
+	"1.3.6.1.4.1.22234.2.5.2.3.1": true,
+
+	// QuoVadis
+	"1.3.6.1.4.1.8024.0.2.100.1.2": true,
+
+	// SECOM Trust Systems
+	"1.2.392.200091.100.721.1": true,
+
+	// Starfield Technologies
+	"2.16.840.1.114414.1.7.23.3": true,
+
+	// StartCom Certification Authority
+	"1.3.6.1.4.1.23223.2":     true,
+	"1.3.6.1.4.1.23223.1.1.1": true,
+
+	// Swisscom
+	"2.16.756.1.83.21.0": true,
+
+	// SwissSign
+	"2.16.756.1.89.1.2.1.1": true,
+
+	// T-Systems
+	"1.3.6.1.4.1.7879.13.24.1": true,
+
+	// Thawte
+	"2.16.840.1.113733.1.7.48.1": true,
+
+	// Trustwave
+	"2.16.840.1.114404.1.1.2.4.1": true,
+
+	// Symantec (VeriSign)
+	"2.16.840.1.113733.1.7.23.6": true,
+
+	// Verizon Business (formerly Cybertrust)
+	"1.3.6.1.4.1.6334.1.100.1": true,
+
+	// Wells Fargo
+	"2.16.840.1.114171.500.9": true,
+}
 
 // rootHandler handles requests to /
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -60,11 +164,13 @@ func validateHost(hostname string) map[string]string {
 	validationResult := ""
 	validationResultShort := ""
 	resultColor := ""
+	resultColorHex := ""
 	message := ""
 
 	// Certs info
 	subjectCommonName := ""
 	subjectOrganization := ""
+	subjectEVOID := ""
 	issuerCommonName := ""
 	issuerOrganization := ""
 
@@ -82,6 +188,7 @@ func validateHost(hostname string) map[string]string {
 		validationResult = "Not Validated"
 		validationResultShort = "!"
 		resultColor = "red"
+		resultColorHex = "#FF1744"
 		message = err.Error()
 	} else {
 		defer conn.Close()
@@ -92,6 +199,7 @@ func validateHost(hostname string) map[string]string {
 
 		subjectCommonName = certInfo["subject_common_name"]
 		subjectOrganization = certInfo["subject_organization"]
+		subjectEVOID = certInfo["subject_ev_oid"]
 		issuerCommonName = certInfo["issuer_common_name"]
 		issuerOrganization = certInfo["issuer_organization"]
 
@@ -99,6 +207,7 @@ func validateHost(hostname string) map[string]string {
 		validationResult = "Domain Control Validated"
 		validationResultShort = "DV"
 		resultColor = "yellow"
+		resultColorHex = "#FF9800"
 		message = "The website operator's control over this domain has been validated."
 
 		// Set to IV if cert has O field
@@ -106,7 +215,17 @@ func validateHost(hostname string) map[string]string {
 			validationResult = "Identity Validated"
 			validationResultShort = "IV"
 			resultColor = "blue"
+			resultColorHex = "#2196F3"
 			message = "The website operator's identity (organization or individual) has been validated."
+		}
+
+		// EV certificate
+		if len(subjectEVOID) > 0 {
+			validationResult = "Extended Validation"
+			validationResultShort = "EV"
+			resultColor = "blue" // For compatibility reason
+			resultColorHex = "#2CBE4E"
+			message = "The website operator's identity (organization) has been validated."
 		}
 	}
 
@@ -122,7 +241,8 @@ func validateHost(hostname string) map[string]string {
 		"subject_organization":    subjectOrganization,
 		"issuer_common_name":      issuerCommonName,
 		"issuer_organization":     issuerOrganization,
-		"result_color":            resultColor,
+		"result_color":            resultColor, // Deprecated
+		"result_color_hex":        resultColorHex,
 		"message":                 message,
 	}
 
@@ -135,6 +255,7 @@ func getCertInfo(cert *x509.Certificate) map[string]string {
 	result := map[string]string{
 		"subject_common_name":  "",
 		"subject_organization": "",
+		"subject_ev_oid":       "",
 		"issuer_common_name":   "",
 		"issuer_organization":  "",
 	}
@@ -155,6 +276,22 @@ func getCertInfo(cert *x509.Certificate) map[string]string {
 
 	if len(issuerOrgs) > 0 {
 		result["issuer_organization"] = issuerOrgs[0]
+	}
+
+	// Check all OIDs to identify EV certificates
+	for _, oid := range cert.PolicyIdentifiers {
+		// Construct OID string
+		var buf bytes.Buffer
+		for idx, num := range oid {
+			buf.WriteString(strconv.Itoa(num))
+			if idx < len(oid)-1 {
+				buf.WriteString(".")
+			}
+		}
+		oidString := buf.String()
+		if ev_oids[oidString] {
+			result["subject_ev_oid"] = oidString
+		}
 	}
 
 	return result
